3d1a2f810287a0c9b6f4ab02178abb5530526f12
MT20041 <mitchell.tenhoope15@gmail.com>
2026-04-20 05:06:25 +0000

    Deploy demo site

--- src/services/firestorePaths.ts
+++
@@ -1,4 +0,0 @@
-export const userPath = (userId: string) => `users/${userId}`;
-export const peptidesPath = (userId: string) => `users/${userId}/peptides`;
-export const logsPath = (userId: string) => `users/${userId}/logs`;
-
